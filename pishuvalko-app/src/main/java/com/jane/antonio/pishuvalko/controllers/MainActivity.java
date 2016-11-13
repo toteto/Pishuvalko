2d70d381eb10afe4caa50871586f4a0862929dbd
janedzumerko <jane.dzumerkoski@gmail.com>
2016-11-13 18:25:34 +0100

    Create main menu (starting screen)

--- pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/MainActivity.java
+++ pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/MainActivity.java
@@ -1,16 +1,82 @@
 package com.jane.antonio.pishuvalko.controllers;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jane.antonio.pishuvalko.R;
 
 /** Activity that will serve as the main navigation for the application. */
 public class MainActivity extends AppCompatActivity {
 
+  private Button startGame;
+  private Button goToSettings;
+  private Button exit;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    startGame = (Button) findViewById(R.id.bt_startGame);
+    goToSettings = (Button) findViewById(R.id.bt_goToSettings);
+    exit = (Button) findViewById(R.id.bt_exit);
+
+    startGame.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), LevelSelectionActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    goToSettings.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), Settings.class);
+            startActivity(intent);
+        }
+    });
+
+    exit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which){
+                        case DialogInterface.BUTTON_POSITIVE:
+
+                            //System.exit(0);
+
+                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+
+                            break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //No button clicked
+                            break;
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+
+        }
+    });
+
+
   }
+
+
 }
---
+++ pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/Settings.java
@@ -0,0 +1,20 @@
+package com.jane.antonio.pishuvalko.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jane.antonio.pishuvalko.R;
+
+/**
+ * Created by janedzumerko on 11/13/16.
+ */
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
