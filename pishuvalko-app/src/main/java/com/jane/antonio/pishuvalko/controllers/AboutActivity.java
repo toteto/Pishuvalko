c2842a41f73fe5769be33899f737c4807a665257
janedzumerko <jane.dzumerkoski@gmail.com>
2017-01-16 23:51:18 +0100

    LevelItem #11 settings

---
+++ pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.jane.antonio.pishuvalko.controllers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jane.antonio.pishuvalko.R;
+
+/**
+ * Created by janedzumerko on 1/15/17.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
--- pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/SettingsActivity.java
+++ pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
@@ -18,41 +19,21 @@ import com.jane.antonio.pishuvalko.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    Switch sound_switch;
-    TextView sound;
-    TextView language;
-    ImageView share_view;
-    private View btnClose;
-
+    Button language;
+    Button about;
+    Button share;
+    View btnClose;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        sound = (TextView) findViewById(R.id.txtv_sound);
-        language = (TextView) findViewById(R.id.txtv_langauge);
-        share_view = (ImageView) findViewById(R.id.share_button);
-        sound_switch = (Switch) findViewById(R.id.soundSwitch);
+        language = (Button) findViewById(R.id.settings_bt_language);
+        about = (Button) findViewById(R.id.settings_bt_about);
+        share = (Button) findViewById(R.id.settings_bt_share);
+
         btnClose = findViewById(R.id.btn_close);
 
-        //set the switch to last choice
-        //sound_switch.setChecked(true);
-
-        //attach a listener to check for changes in state
-        sound_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-
-                if(isChecked){
-                    Toast.makeText(getApplicationContext(), "ON", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "OFF", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
 
         language.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,15 +42,15 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        share_view.setOnClickListener(new View.OnClickListener() {
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 String appLink = "https://play.google.com";
-                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Pishuvalko");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Пишувалко");
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, appLink);
                 sharingIntent.setType("text/plain");
-                startActivity(Intent.createChooser(sharingIntent, "Spodeli preku ..."));
+                startActivity(Intent.createChooser(sharingIntent, getString(R.string.settings_share_via)));
             }
         });
 
@@ -77,6 +58,14 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
             }
         });
 
