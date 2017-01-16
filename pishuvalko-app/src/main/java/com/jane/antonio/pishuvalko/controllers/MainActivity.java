6b4e85cb415fd05e64c7ef7438da8e3344acd5a3
toteto <toteto@users.noreply.github.com>
2017-01-16 23:13:23 +0100

    Verification check when trying to enter ParentsActivity

--- pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/MainActivity.java
+++ pishuvalko-app/src/main/java/com/jane/antonio/pishuvalko/controllers/MainActivity.java
@@ -3,14 +3,24 @@ package com.jane.antonio.pishuvalko.controllers;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jane.antonio.pishuvalko.R;
 
-/** Activity that will serve as the main navigation for the application. */
+import java.lang.ref.WeakReference;
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Activity that will serve as the main navigation for the application.
+ */
 public class MainActivity extends AppCompatActivity {
 
   private Button startGame;
@@ -64,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
         builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No",
-          dialogClickListener).show();
+                dialogClickListener).show();
 
       }
     });
@@ -72,9 +82,52 @@ public class MainActivity extends AppCompatActivity {
     findViewById(R.id.btnParents).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        startActivity(new Intent(MainActivity.this, ParentsActivity.class));
+        showParentsQuizDialog();
       }
     });
+  }
 
+  private void showParentsQuizDialog() {
+    final EditText etAnswer = new EditText(this);
+    etAnswer.setInputType(InputType.TYPE_CLASS_NUMBER);
+    etAnswer.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+    final Random rnd = new Random();
+    final int x = rnd.nextInt(10) + 3;
+    final int y = rnd.nextInt(10) + 3;
+    final WeakReference<EditText> etAnswerWeakReference = new WeakReference<>(etAnswer);
+    new AlertDialog.Builder(this, R.style.SolutionPopupTheme)
+            .setTitle(R.string.confirm_that_you_are_the_parent)
+            .setMessage(String.format(Locale.getDefault(), "%s\n%d * %d", getString(R.string.please_answer), x, y))
+            .setView(etAnswer)
+            .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialogInterface, int i) {
+                if (etAnswerWeakReference.get() != null) {
+                  try {
+                    final int answer = Integer.parseInt(etAnswerWeakReference.get().getText().toString().trim());
+                    if (answer == x * y) {
+                      startActivity(new Intent(MainActivity.this, ParentsActivity.class));
+                    } else {
+                      showIncorrectParentsAnswer();
+                    }
+                  } catch (NumberFormatException e) {
+                    showIncorrectParentsAnswer();
+                  } finally {
+                    dialogInterface.dismiss();
+                  }
+                }
+              }
+            })
+            .setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+              }
+            })
+            .show();
+  }
+
+  private void showIncorrectParentsAnswer() {
+    Toast.makeText(this, getString(R.string.incorrect_answer), Toast.LENGTH_SHORT).show();
   }
 }
